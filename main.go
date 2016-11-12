333e7338f0836bfdf879360959dcab5eb99e3a48
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-12 09:21:28 -0800

    Start handler for actual fetching of feeds

--- main.go
+++ main.go
@@ -3,13 +3,37 @@ package main
 import (
     "net/http"
     "fmt"
+    "encoding/json"
 )
 
 func handlePing(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "OK")
 }
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    type Input struct {
+        FeedUrl string `json:"feed_url"`
+    }
+
+    if r.Method != http.MethodPost {
+        http.Error(w, "Not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    var inputs []Input
+    err := json.NewDecoder(r.Body).Decode(&inputs)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+    }
+
+    for _, input := range inputs {
+        fmt.Fprintf(w, "%s", input.FeedUrl)
+    }
+}
+
 func init() {
     http.HandleFunc("/ping", handlePing)
+    http.HandleFunc("/handle", handleRequest)
 }
 
