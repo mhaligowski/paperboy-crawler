d35f7306e8c995d86c5415f00c79243069652cdb
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-19 08:23:04 -0800

    Get the actual feed value

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
     "net/http"
     "fmt"
+    "io/ioutil"
+
+    "google.golang.org/appengine"
+    "google.golang.org/appengine/urlfetch"
 )
 
 func handlePing(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "OK")
 }
 
-type Input struct {
+type input struct {
     FeedId string
     FeedUrl string
 }
@@ -20,20 +24,32 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    input, err := parseInput(r)
-
-    if err != nil {
+    input, err := parseInput(r); if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
     }
 
-    fmt.Fprintf(w, "%s", input.FeedUrl)
+    ctx := appengine.NewContext(r)
+    client := urlfetch.Client(ctx)
+    response, err := client.Get(input.FeedUrl); if err != nil {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+
+    defer response.Body.Close()
+    body, err := ioutil.ReadAll(response.Body); if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Fprintf(w, "%q", body)
+
 }
 
-func parseInput(r *http.Request) (i Input, err error) {
+func parseInput(r *http.Request) (i input, err error) {
     feed_id := r.FormValue("feed_id")
     feed_url := r.FormValue("feed_url")
 
-    return Input{feed_id, feed_url}, nil
+    return input{feed_id, feed_url}, nil
 }
 
 func init() {
